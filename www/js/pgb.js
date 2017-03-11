6baa0c41ac7dad77b7787093de13bf30f33247f8
Kruszec Łukasz <lukasz.kruszec@microsoft.wsei.edu.pl>
2017-03-11 16:34:08 +0100

    First commit

---
+++ www/js/pgb.js
@@ -0,0 +1,25 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	checkConnection();
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+	document.body.innerHTML = states[networkState]; 
+}
+ 
+checkConnection();
